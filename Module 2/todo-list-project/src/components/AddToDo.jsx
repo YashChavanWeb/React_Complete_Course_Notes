051752179f0a7fa1f400da11e910213dd83cf85f
Yash Chavan <yashchavan4628@gmail.com>
2024-06-24 15:52:02 +0530

    forms and useRef hook. Now till here everything is perfect. all the projects and even the chapters. now will start with context api.

--- Module 2/todo-list-project/src/components/AddToDo.jsx
+++ Module 2/todo-list-project/src/components/AddToDo.jsx
@@ -1,22 +1,36 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 function AddTodo({ onAddButton }) {
 
 
-    const [todoName, setTodoName] = useState('')
-    const [todoDate, setTodoDate] = useState('')
+    // const [todoName, setTodoName] = useState('')
+    // const [todoDate, setTodoDate] = useState('')
 
-    const handleInputChange = (e) => {
-        setTodoName(e.target.value)
-    }
-    const handleDateChange = (e) => {
-        setTodoDate(e.target.value)
-    }
+    const elementName = useRef();
+    const elementDate = useRef();
+
+
+    // const handleInputChange = (e) => {
+    //     setTodoName(e.target.value)
+    // }
+    // const handleDateChange = (e) => {
+    //     setTodoDate(e.target.value)
+    // }
 
     const handleAddButtonClicked = () => {
-        onAddButton(todoName, todoDate)
-        setTodoName('');
-        setTodoDate('');
+
+        let myName = elementName.current.value;
+        let myDate = elementDate.current.value;
+
+        onAddButton(myName, myDate);
+
+        myName = ''
+        myDate = ''
+
+
+
+        // setTodoName('');
+        // setTodoDate('');
     }
 
 
@@ -24,11 +38,20 @@ function AddTodo({ onAddButton }) {
 
     return (
         <div className="add-todo">
-            <div className="input-group">
+
+
+            {/* <div className="input-group">
                 <input type="text" placeholder="Enter Todo Here" value={todoName} style={{ margin: '20px' }} onChange={handleInputChange} />
             </div>
             <div className="input-group">
                 <input type="date" style={{ margin: '20px' }} value={todoDate} onChange={handleDateChange} />
+            </div> */}
+
+            <div className="input-group">
+                <input type="text" placeholder="Enter Todo Here" style={{ margin: '20px' }} ref={elementName} />
+            </div>
+            <div className="input-group">
+                <input type="date" style={{ margin: '20px' }} ref={elementDate} />
             </div>
             <div className="input-group">
                 <button type="button" style={{ margin: '20px' }}
